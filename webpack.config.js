59862d8918ff2cd9accacc6730c00f33ac276c15
Jesse Abramson <jabramson@connected.io>
2020-06-13 15:02:06 -0400

    handle no dotenv in prod

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const webpack = require("webpack");
 
 const outputDirectory = "dist";
 
-const env = dotenv.config().parsed;
+const env = dotenv.config().parsed || {};
 const envKeys = Object.keys(env).reduce((prev, next) => {
   prev[`process.env.${next}`] = JSON.stringify(env[next]);
   return prev;
