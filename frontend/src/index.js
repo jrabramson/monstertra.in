4ad1bd5d4ccc570cebb24a58a5953f4a1a252b1f
Jesse Abramson <jabramson@connected.io>
2020-06-14 22:02:59 -0400

    Loader

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Loadable from 'react-loadable';
+import styled from 'styled-components';
+
+const LoadingWrapper = styled.div`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  background: black;
+  z-index: 999;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Loading = ({}) => {
+  return <LoadingWrapper>
+    <h2>Loading...</h2>
+  </LoadingWrapper>
+}
+
+const App = Loadable({
+  loader: () => import('./App.js'),
+  loading: Loading
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
