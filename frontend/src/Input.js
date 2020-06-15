66d79e7dffe1c16ebd7bbfb09ffe6ff4b001fd3d
Jesse Abramson <jabramson@connected.io>
2020-06-15 10:55:05 -0400

    oops

--- frontend/src/Input.js
+++ frontend/src/Input.js
@@ -171,7 +171,7 @@ export default ({
             type="text"
             value={name}
             maxLength={15}
-            onChange={(e) => onChange("name", e.target.value.replace('<p data-f-id="pbf" style="text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;">Powered by <a href="https://www.froala.com/wysiwyg-editor?pb=1" title="Froala Editor">Froala Editor</a></p>', ''))}
+            onChange={(e) => onChange("name", e.target.value)}
           />
         </InputOption>
         <InputOption>
@@ -240,7 +240,7 @@ export default ({
       <FroalaEditor
         tag="textarea"
         model={description}
-        onModelChange={(val) => onChange("description", val)}
+        onModelChange={(val) => onChange("description", val.replace('<p data-f-id="pbf" style="text-align: center; font-size: 14px; margin-top: 30px; opacity: 0.65; font-family: sans-serif;">Powered by <a href="https://www.froala.com/wysiwyg-editor?pb=1" title="Froala Editor">Froala Editor</a></p>', ''))}
         config={{
           placeholderText: "Edit Your Content Here!",
           pluginsEnabled: ["image", "imageManager"],
